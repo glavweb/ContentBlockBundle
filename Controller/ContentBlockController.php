1a924e6b8668b21befaa6444527a2795bb01013c
Fafnur <fafnur@yandex.ru>
2016-03-30 15:19:37 +0600

    Added API for create, show, edit, delete contentBlocks

--- Controller/ContentBlockController.php
+++ Controller/ContentBlockController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Glavweb\ContentBlockBundle\Controller;
 
+use Glavweb\ContentBlockBundle\Entity\ContentBlock;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -44,5 +45,101 @@ class ContentBlockController extends Controller
         return new JsonResponse(array(
             'result' => $result
         ));
+    }
+
+    /**
+     * @Route("/api/content-block/{name}", name="content_block_create", requirements={"_method": "POST"})
+     *
+     * @param string $name
+     * @return JsonResponse
+     */
+    public function createAction($name)
+    {
+        $em           = $this->getDoctrine()->getManager();
+        $repository   = $em->getRepository('GlavwebContentBlockBundle:ContentBlock');
+        $contentBlock = $repository->findByName($name);
+
+        if(empty($contentBlock)) {
+            $contentBlock = new ContentBlock();
+            $contentBlock->setName($name);
+            $contentBlock->setBody($name);
+            $em->persist($contentBlock);
+            $em->flush();
+            $status = 201;
+        } else {
+            $status = 400;
+        }
+
+        return new JsonResponse(array(
+            'status' =>  $status == 201 ? true : false
+        ), $status);
+    }
+
+    /**
+     * @Route("/api/content-block/{name}", name="content_block_show", requirements={"_method": "GET"})
+     *
+     * @param string $name
+     * @return JsonResponse
+     */
+    public function showAction($name)
+    {
+        $em           = $this->getDoctrine()->getManager();
+        $repository   = $em->getRepository('GlavwebContentBlockBundle:ContentBlock');
+        $contentBlock = $repository->findByName($name);
+
+        if(!empty($contentBlock)) {
+            $status = 200;
+        } else {
+            $status = 400;
+        }
+        return new JsonResponse(array(
+            'status' =>  $status == 200 ? true : false
+        ), $status);
+    }
+
+    /**
+     * @Route("/api/content-block/{id}", name="content_block_edit", requirements={"_method": "PUT"})
+     *
+     * @param string $name
+     * @return JsonResponse
+     */
+    public function editAction($name)
+    {
+        $em           = $this->getDoctrine()->getManager();
+        $repository   = $em->getRepository('GlavwebContentBlockBundle:ContentBlock');
+        $contentBlock = $repository->findByName($name);
+
+        if(!empty($contentBlock)) {
+            $status = 200;
+        } else {
+            $status = 400;
+        }
+        return new JsonResponse(array(
+            'status' =>  $status == 200 ? true : false
+        ), $status);
+    }
+
+    /**
+     * @Route("/api/content-block/{name}", name="content_block_remove", requirements={"_method": "DELETE"})
+     *
+     * @param string $name
+     * @return JsonResponse
+     */
+    public function removeAction($name)
+    {
+        $em           = $this->getDoctrine()->getManager();
+        $repository   = $em->getRepository('GlavwebContentBlockBundle:ContentBlock');
+        $contentBlock = $repository->findByName($name);
+
+        if(!empty($contentBlock)) {
+            $em->remove($contentBlock);
+            $em->flush();
+            $status = 204;
+        } else {
+            $status = 400;
+        }
+        return new JsonResponse(array(
+            'status' =>  $status == 200 ? true : false
+        ), $status);
     }
 }
